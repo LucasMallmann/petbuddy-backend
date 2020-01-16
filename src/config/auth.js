e4775580c7e21c092f66bdf68cf0dc934f4fbdbd
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-16 17:36:59 -0300

    feat: user session

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'petbuddyauthjwt',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 const routes = Router();
+
+routes.get('/sessions', SessionController.store);
 
 routes.post('/users', UserController.store);
 
