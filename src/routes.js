c620601377255eea7c885a5748ad4888e969335f
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-16 19:27:08 -0300

    feat: upload avatar

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import AvatarController from './app/controllers/AvatarController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.get('/sessions', SessionController.store);
 
@@ -13,5 +17,6 @@ routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/avatars', upload.single('file'), AvatarController.store);
 
 export default routes;
